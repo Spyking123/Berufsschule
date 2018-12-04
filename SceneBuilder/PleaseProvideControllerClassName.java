eae5509d17af98966bd2cf9938f21b6c7a8dd5b0
ilravenll <raven-nevar@gmx.de>
2018-12-04 10:36:06 +0100

    Hinzufügen der Main GUI und "PleaseProvideControllerClassName"

---
+++ SceneBuilder/PleaseProvideControllerClassName.java
@@ -0,0 +1,166 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private GridPane GrodPaneRoom;
+
+    @FXML
+    private Label PrinterNumber;
+
+    @FXML
+    private Label ComputerNumber;
+
+    @FXML
+    private TextField RoomID;
+
+    @FXML
+    private Label MonitorNumber;
+
+    @FXML
+    private Label BeamerNumber;
+
+    @FXML
+    private Button ShowNumbers;
+
+    @FXML
+    private GridPane GridPaneBook;
+
+    @FXML
+    private TextField BookID;
+
+    @FXML
+    private Label BookAutor;
+
+    @FXML
+    private Label BookLesson;
+
+    @FXML
+    private Label BookNumber;
+
+    @FXML
+    private Button ShowBook;
+
+    @FXML
+    private Label AusgeliehenNumber;
+
+    @FXML
+    private GridPane GrodPaneRoomAdd;
+
+    @FXML
+    private TextField RoomBeamerAdd;
+
+    @FXML
+    private TextField RoomIDAdd;
+
+    @FXML
+    private Button AddRoom;
+
+    @FXML
+    private TextField RoomMonitorAdd;
+
+    @FXML
+    private TextField RoomComputerAdd;
+
+    @FXML
+    private TextField RoomPrinterAdd;
+
+    @FXML
+    private GridPane GridPaneBookAdd;
+
+    @FXML
+    private TextField BookAutorAdd;
+
+    @FXML
+    private TextField BookIDAdd;
+
+    @FXML
+    private Button AddBook;
+
+    @FXML
+    private TextField BookLessonAdd;
+
+    @FXML
+    private TextField BookAusgeliehenAdd;
+
+    @FXML
+    private TextField BookNumberAdd;
+
+    @FXML
+    private GridPane GrodPaneRoomChange;
+
+    @FXML
+    private TextField RoomBeamerChange;
+
+    @FXML
+    private TextField RoomIDChange;
+
+    @FXML
+    private Button ChangeRoom;
+
+    @FXML
+    private TextField RoomMonitorChange;
+
+    @FXML
+    private TextField RoomComputerChange;
+
+    @FXML
+    private TextField RoomPrinterChange;
+
+    @FXML
+    private GridPane GridPaneBookChange;
+
+    @FXML
+    private TextField BookAutorChange;
+
+    @FXML
+    private TextField BookIDChange;
+
+    @FXML
+    private Button ChangeBook;
+
+    @FXML
+    private TextField BookLessonChange;
+
+    @FXML
+    private TextField BookAusgeliehenChange;
+
+    @FXML
+    private TextField BookNumberChange;
+
+    @FXML
+    void RunAddBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RunAddRoom(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RunChangeBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RunChangeRoom(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RunShowBooks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RunShowNumbers(ActionEvent event) {
+
+    }
+
+}
+
